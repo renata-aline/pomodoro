4ba6a70a2e50703dfa610b9e3ba2520b73087a0f
renata-aline <renatalinec@gmail.com>
2023-11-28 17:15:13 -0300

    texto da página alterado

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-const html = document.querySelector('html')
-const focoBt = document.querySelector('.app__card-button--foco')
-const curtoBt = document.querySelector('.app__card-button--curto')
-const longoBt = document.querySelector('.app__card-button--longo')
-const banner = document.querySelector('.app__image')
+const html = document.querySelector("html");
+const focoBt = document.querySelector(".app__card-button--foco");
+const curtoBt = document.querySelector(".app__card-button--curto");
+const longoBt = document.querySelector(".app__card-button--longo");
+const banner = document.querySelector(".app__image");
+const title = document.querySelector(".app__title");
 
+focoBt.addEventListener("click", () => {
+  alterarContexto("foco");
+});
 
-focoBt.addEventListener('click', () => {
-   alterarContexto('foco')
-})
+curtoBt.addEventListener("click", () => {
+  alterarContexto("descanso-curto");
+});
 
-curtoBt.addEventListener('click', () => {
-    alterarContexto('descanso-curto')
-})
-
-longoBt.addEventListener('click', () => {
-    alterarContexto('descanso-longo')
-})
+longoBt.addEventListener("click", () => {
+  alterarContexto("descanso-longo");
+});
 
 function alterarContexto(contexto) {
-    html.setAttribute('data-contexto', contexto)
-    banner.setAttribute('src', `/imagens/${contexto}.png`)
+  html.setAttribute("data-contexto", contexto);
+  banner.setAttribute("src", `/imagens/${contexto}.png`);
+  switch (contexto) {
+    case "foco":
+      title.innerHTML = `Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+
+      break;
+    case "descanso-curto":
+      title.innerHTML = `Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+
+      break;
+    case "descanso-longo":
+      title.innerHTML = ` Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>`;
+
+    default:
+      break;
+  }
 }
