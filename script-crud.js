1041a3397a86ec53116bb73db424578ec3459a42
renata-aline <renatalinec@gmail.com>
2023-12-05 15:32:31 -0300

    submetendo o formulário

--- script-crud.js
+++ script-crud.js
@@ -1,8 +1,20 @@
 
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionaTarefa = document.querySelector('.app__form-add-task')
+const textarea = document.querySelector('.app__form-textarea')
+
+const tarefas = []
 
 
-btnAdicionarTarefa.addEventListener('click',() =>{
+btnAdicionarTarefa.addEventListener('click',() => {
     formAdicionaTarefa.classList.toggle('hidden')
+})
+
+formAdicionaTarefa.addEventListener('submit',(evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descricao: textarea.value
+    }
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas',tarefas)
 })
